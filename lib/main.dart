a9556bdfbabb5f04feb9c6c6e6dfe119c4074712
LeThiYenLuaB1910408 <luaB1910408@student.ctu.edu.vn>
2022-10-16 23:27:43 +0700

    Quan ly trang thai voi provider - ProductsManager

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'ui/screens.dart';
 
 void main() {
@@ -11,45 +12,52 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My Shop',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          fontFamily: 'Lato',
-          colorScheme: ColorScheme.fromSwatch(
-            primarySwatch: Colors.blue,
-          ).copyWith(
-            secondary: Colors.deepOrange,
-          )),
-      // home: const SafeArea(
-      //   // child: ProductDetailScreen(
-      //   //   ProductsManager().items[0],
-      //   //),
-      //   //   child: ProductsOverviewScreen(),
-      //   // ),
-      //   // child: UserProductScreen(),
-      //   // child: CartScreen(),
-      //   child: OrdersScreen(),
-      // ),
-      home: const ProductsOverviewScreen(),
-      routes: {
-        CartScreen.routeName: (ctx) => const CartScreen(),
-        OrdersScreen.routeName: (ctx) => const OrdersScreen(),
-        UserProductScreen.routeName: (ctx) => const UserProductScreen(),
-      },
-      onGenerateRoute: (settings) {
-        if (settings.name == ProductDetailScreen.routeName) {
-          final productId = settings.arguments as String;
-          return MaterialPageRoute(
-            builder: (ctx) {
-              return ProductDetailScreen(
-                ProductsManager().findById(productId),
-              );
-            },
-          );
-        }
-        return null;
-      },
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => ProductsManager(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'My Shop',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            fontFamily: 'Lato',
+            colorScheme: ColorScheme.fromSwatch(
+              primarySwatch: Colors.blue,
+            ).copyWith(
+              secondary: Colors.deepOrange,
+            )),
+        // home: const SafeArea(
+        //   // child: ProductDetailScreen(
+        //   //   ProductsManager().items[0],
+        //   //),
+        //   //   child: ProductsOverviewScreen(),
+        //   // ),
+        //   // child: UserProductScreen(),
+        //   // child: CartScreen(),
+        //   child: OrdersScreen(),
+        // ),
+        home: const ProductsOverviewScreen(),
+        routes: {
+          CartScreen.routeName: (ctx) => const CartScreen(),
+          OrdersScreen.routeName: (ctx) => const OrdersScreen(),
+          UserProductScreen.routeName: (ctx) => const UserProductScreen(),
+        },
+        onGenerateRoute: (settings) {
+          if (settings.name == ProductDetailScreen.routeName) {
+            final productId = settings.arguments as String;
+            return MaterialPageRoute(
+              builder: (ctx) {
+                return ProductDetailScreen(
+                  ctx.read<ProductsManager>().findById(productId),
+                );
+              },
+            );
+          }
+          return null;
+        },
+      ),
     );
   }
 }
--- lib/ui/products/user_products_screen.dart
+++ lib/ui/products/user_products_screen.dart
@@ -1,48 +1,51 @@
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
 import 'user_product_list_tile.dart';
 import 'products_manager.dart';
 import '../shared/app_drawer.dart';
 
-class UserProductScreen extends StatelessWidget{
+class UserProductScreen extends StatelessWidget {
   static const routeName = '/user-products';
   const UserProductScreen({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     final productsManager = ProductsManager();
     return Scaffold(
-      appBar: AppBar(
-        title:  const Text('Your Products'),
-        actions: <Widget>[
-          buildAddButton(),
-        ],
-      ),
-      drawer: const AppDrawer(),
-      body: RefreshIndicator(
-        onRefresh: () async => print('refresh products'),
-        child: buildUserProductListView(productsManager),
-      )
+        appBar: AppBar(
+          title: const Text('Your Products'),
+          actions: <Widget>[
+            buildAddButton(),
+          ],
+        ),
+        drawer: const AppDrawer(),
+        body: RefreshIndicator(
+          onRefresh: () async => print('refresh products'),
+          child: buildUserProductListView(productsManager),
+        ));
+  }
+
+  Widget buildUserProductListView(ProductsManager productsManager) {
+    return Consumer<ProductsManager>(
+      builder: (ctx, productsManager, child) {
+        return ListView.builder(
+          itemCount: productsManager.itemCount,
+          itemBuilder: (ctx, i) => Column(
+            children: [
+              UserProductListTile(productsManager.items[i]),
+              const Divider(),
+            ],
+          ),
+        );
+      },
     );
-
-  }
-  Widget buildUserProductListView(ProductsManager productsManager){
-    return ListView.builder(
-      itemCount: productsManager.itemCount,
-      itemBuilder: (ctx, i)=> Column(
-        children: [
-          UserProductListTile(productsManager.items[i]),
-          const Divider(),
-        ],
-      ),
-    );
   }
 
-  Widget buildAddButton(){
-    return IconButton(onPressed: (){
-      print('Go to edit product screen');
-    }, icon: const Icon(Icons.add));
+  Widget buildAddButton() {
+    return IconButton(
+        onPressed: () {
+          print('Go to edit product screen');
+        },
+        icon: const Icon(Icons.add));
   }
-
-
 }
