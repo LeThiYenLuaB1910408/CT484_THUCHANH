1b82f843108831bd4bd8c19dabc92bcb69adf11c
LeThiYenLuaB1910408 <luaB1910408@student.ctu.edu.vn>
2022-10-17 00:27:00 +0700

    Quan ly trang thai voi provider - OrdersManager

--- lib/ui/products/product_grid_tile.dart
+++ lib/ui/products/product_grid_tile.dart
@@ -71,7 +71,7 @@ class ProductGridTile extends StatelessWidget {
             ..showSnackBar(
               SnackBar(
                 content: const Text('Item added to cart'),
-                duration: const Duration(seconds: 10),
+                duration: const Duration(seconds: 2),
                 action: SnackBarAction(
                   label: 'UNDO',
                   onPressed: () {
